3970a718b21caaad8867ad1dc1d6790513805544
Meghana D <reddymeghana33662@gmail.com>
2026-05-04 22:31:34 +0530

    Updated Print Statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello DevOps using Git & Jenkins!");
+        System.out.println("DevOps is blend of Development and Integration");
     }
 }
